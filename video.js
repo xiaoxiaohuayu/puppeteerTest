97adf6092b40b6873532e0ca3f2ef64b22957801
Hao <1521183864@qq.com>
2020-11-17 18:55:52 +0800

    no message

--- video.js
+++ video.js
@@ -7,11 +7,24 @@ const puppeteer = require('puppeteer');
     await page.waitFor(1000)
         // console.log(await page.$eval("#player3 > ul"))
     const date_cnv = await page.$$eval('#player3 ul li a', url => {
-        var dizhi = {};
+        var dizhi = [];
         for (var i = 0; i < url.length; i++) {
-            dizhi[i] = url[i].href += ',' + url[i].innerText;
+            // dizhi.push(url[i].href += ',' + url[i].innerText);
+            dizhi.push(url[i].href);
         }
         return dizhi;
     });
-    console.log(date_cnv)
+    // await page.$eval('script:nth-child(11)', e => e.outerHTML))
+    console.log(await page.$$eval('script', el => el.map(el => el.innerHTML)))
+        // for (var i = 0; i < date_cnv.length; i++) {
+        //     console.log(date_cnv[i], '21321321321')
+        //     await page.goto(date_cnv[i])
+        //     await page.waitFor(500)
+        // await page.$$eval('script', mu3 => {
+        //     for (var l = 0; l < mu3.length; l++) {
+        //         console.log(mu3[i].innerHTML)
+        //     }
+        // })
+        // }
+        // console.log(date_cnv)
 })()
